e3b0917095b28cce27b2d59d48fab1c774694267
Phạm Tuấn Hưng <phamtuanhung521@gmail.com>
2026-05-05 21:50:22 +0700

    Add files via upload

--- src/main/java/com/expensemanager/Main.java
+++ src/main/java/com/expensemanager/Main.java
@@ -1,17 +1,19 @@
 package com.expensemanager;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import com.expensemanager.entity.*;
+import com.expensemanager.database.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test kết nối
+        try {
+            Connection conn = DatabaseUtil.getConnection();
+            System.out.println("Kết nối MySQL thành công!");
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Lỗi kết nối: " + e.getMessage());
         }
     }
 }
